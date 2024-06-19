a4b746929128dad29312f4c9c1ceb54701c3c78e
Viktor Miric <viktorm47@gmail.com>
2024-06-19 17:58:43 +0200

    moved data queries into separate file, showing images specific to the user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,39 @@
-import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { getUserImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (images, { asc }) => [asc(images.createdAt)],
-  });
+async function Images() {
+  const images = await getUserImages();
 
   return (
-    <main className="">
-      <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] justify-items-center gap-5">
-        {images.map((image) => (
-          <div key={image.id}>
-            <div className="aspect-video w-80 overflow-hidden rounded-sm">
-              <img
-                src={image.url}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <p>{image.name}</p>
+    <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] justify-items-center gap-5">
+      {images.map((image) => (
+        <div key={image.id}>
+          <div className="aspect-video w-80 overflow-hidden rounded-sm">
+            <img
+              src={image.url}
+              className="h-full w-full object-cover object-center"
+            />
           </div>
-        ))}
-      </div>
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="mt-8 text-center text-xl font-semibold">
+          Please sing in.
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
