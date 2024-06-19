bc76c0d0b38ef3a72987d244f96d5ca29a068d08
Viktor Miric <viktorm47@gmail.com>
2024-06-19 16:26:47 +0200

    image uploading enabled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import "~/styles/globals.css";
 import "@uploadthing/react/styles.css";
 
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "~/app/api/uploadthing/core";
+
 import { Inter } from "next/font/google";
 import TopNav from "../components/top-nav";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -22,6 +26,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={`${inter.className} dark`}>
         <body>
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
           <TopNav />
           {children}
         </body>
--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between border-b border-zinc-400 p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -10,7 +15,12 @@ export default function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton endpoint="imageUploader" />
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
